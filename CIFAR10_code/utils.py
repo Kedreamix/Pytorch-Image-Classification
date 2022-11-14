00004bdfa9cea94f1436bb9c62d5d2f62f75f97d
Dreaming-future <1016617094@qq.com>
2022-11-14 19:19:30 +0800

    更新v3.0版本的工具函数和依赖

--- CIFAR10_code/utils.py
+++ CIFAR10_code/utils.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm # 在脚本中使用较好
+# from tqdm import tqdm_notebook as tqdm # 在jupyter中使用
+
 
 def get_mean_and_std(dataset):
     '''Compute the mean and std value of dataset.'''
@@ -23,7 +26,7 @@ def get_acc(outputs, label):
     total = outputs.shape[0]
     probs, pred_y = outputs.data.max(dim=1) # 得到概率
     correct = (pred_y == label).sum().data
-    return correct / total
+    return torch.div(correct, total)
 
 
 class EarlyStopping:
@@ -68,14 +71,13 @@ class EarlyStopping:
         # torch.save(model.state_dict(), 'checkpoint.pt')	# 这里会存储迄今最优模型的参数
         self.val_loss_min = val_loss
 
-def train(net, trainloader, testloader, epoches, optimizer , criterion, scheduler , path = './model.pth', writer = None ,verbose = False):
+def train(net, trainloader, testloader, epochs, optimizer , criterion, scheduler , path = './model.pth', writer = None ,verbose = False):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     best_acc = 0
     train_acc_list, test_acc_list = [],[]
     train_loss_list, test_loss_list = [],[]
     lr_list  = []
-    for i in range(epoches):
-        start = time.time()
+    for i in range(epochs):
         train_loss = 0
         train_acc = 0
         test_loss = 0
@@ -83,41 +85,36 @@ def train(net, trainloader, testloader, epoches, optimizer , criterion, schedule
         if torch.cuda.is_available():
             net = net.to(device)
         net.train()
-        for step,data in enumerate(trainloader,start=0):
-            im,label = data
-            im = im.to(device)
-            label = label.to(device)
+        train_step = len(trainloader)
+        with tqdm(total=train_step,desc=f'Train Epoch {i + 1}/{epochs}',postfix=dict,mininterval=0.3) as pbar:
+            for step,data in enumerate(trainloader,start=0):
+                im,label = data
+                im = im.to(device)
+                label = label.to(device)
 
-            optimizer.zero_grad()
-            # 释放内存
-            if hasattr(torch.cuda, 'empty_cache'):
-                torch.cuda.empty_cache()
-            # formard
-            outputs = net(im)
-            loss = criterion(outputs,label)
-            # backward
-            loss.backward()
-            # 更新参数
-            optimizer.step()
-
-            train_loss += loss.data
-            # probs, pred_y = outputs.data.max(dim=1) # 得到概率
-            # # 正确的个数
-            # train_acc += (pred_y==label).sum().item()
-            # # 总数
-            # total += label.size(0)
-            train_acc += get_acc(outputs,label)
-            # 打印下载进度
-            rate = (step + 1) / len(trainloader)
-            a = "*" * int(rate * 50)
-            b = "." * (50 - int(rate * 50))
-            print('\r train {:3d}|{:3d} {:^3.0f}%  [{}->{}] '.format(i+1,epoches,int(rate*100),a,b),end='')
+                optimizer.zero_grad()
+                # 释放内存
+                if hasattr(torch.cuda, 'empty_cache'):
+                    torch.cuda.empty_cache()
+                # formard
+                outputs = net(im)
+                loss = criterion(outputs,label)
+                # backward
+                loss.backward()
+                # 更新参数
+                optimizer.step()
+                # 累计损失
+                train_loss += loss.item()
+                train_acc += get_acc(outputs,label).item()
+                pbar.set_postfix(**{'Train Acc' : train_acc/(step+1),
+                                'Train Loss' :train_loss/(step+1)})
+                pbar.update(1)
+            pbar.close()
         train_loss = train_loss / len(trainloader)
         train_acc = train_acc * 100 / len(trainloader)
         if verbose:
-            train_acc_list.append(train_acc.item())
-            train_loss_list.append(train_loss.item())
-    #     print('train_loss:{:.6f} train_acc:{:3.2f}%' .format(train_loss ,train_acc),end=' ')  
+            train_acc_list.append(train_acc)
+            train_loss_list.append(train_loss)
         # 记录学习率
         lr = optimizer.param_groups[0]['lr']
         if verbose:
@@ -126,52 +123,43 @@ def train(net, trainloader, testloader, epoches, optimizer , criterion, schedule
         scheduler.step(train_loss)
         if testloader is not None:
             net.eval()
+            test_step = len(testloader)
             with torch.no_grad():
-                for step,data in enumerate(testloader,start=0):
-                    im,label = data
-                    im = im.to(device)
-                    label = label.to(device)
-                    # 释放内存
-                    if hasattr(torch.cuda, 'empty_cache'):
-                        torch.cuda.empty_cache()
-                    outputs = net(im)
-                    loss = criterion(outputs,label)
-                    test_loss += loss.data
-                    # probs, pred_y = outputs.data.max(dim=1) # 得到概率
-                    # test_acc += (pred_y==label).sum().item()
-                    # total += label.size(0)
-                    test_acc += get_acc(outputs,label)
-                    rate = (step + 1) / len(testloader)
-                    a = "*" * int(rate * 50)
-                    b = "." * (50 - int(rate * 50))
-                    print('\r test  {:3d}|{:3d} {:^3.0f}%  [{}->{}] '.format(i+1,epoches,int(rate*100),a,b),end='')
-            test_loss = test_loss / len(testloader)
-            test_acc = test_acc * 100 / len(testloader)
+                with tqdm(total=test_step,desc=f'Test Epoch {i + 1}/{epochs}',postfix=dict,mininterval=0.3) as pbar:
+                    for step,data in enumerate(testloader,start=0):
+                        im,label = data
+                        im = im.to(device)
+                        label = label.to(device)
+                        # 释放内存
+                        if hasattr(torch.cuda, 'empty_cache'):
+                            torch.cuda.empty_cache()
+                        outputs = net(im)
+                        loss = criterion(outputs,label)
+                        test_loss += loss.item()
+                        test_acc += get_acc(outputs,label).item()
+                        pbar.set_postfix(**{'Test Acc' : test_acc/(step+1),
+                                'Test Loss' :test_loss/(step+1)})
+                        pbar.update(1)
+                    pbar.close()
+                test_loss = test_loss / len(testloader)
+                test_acc = test_acc * 100 / len(testloader)
             if verbose:
-                test_loss_list.append(test_loss.item())
-                test_acc_list.append(test_acc.item())
-            end = time.time()
+                test_loss_list.append(test_loss)
+                test_acc_list.append(test_acc)
             print(
-                '\rEpoch [{:>3d}/{:>3d}]  Train Loss:{:>.6f}  Train Acc:{:>3.2f}% Test Loss:{:>.6f}  Test Acc:{:>3.2f}%  Learning Rate:{:>.6f}'.format(
-                    i + 1, epoches, train_loss, train_acc, test_loss, test_acc,lr), end='')
+                'Epoch [{:>3d}/{:>3d}]  Train Loss:{:>.6f}  Train Acc:{:>3.2f}% Test Loss:{:>.6f}  Test Acc:{:>3.2f}%  Learning Rate:{:>.6f}'.format(
+                    i + 1, epochs, train_loss, train_acc, test_loss, test_acc,lr))
         else:
-            end = time.time()
-            print('\rEpoch [{:>3d}/{:>3d}]  Train Loss:{:>.6f}  Train Acc:{:>3.2f}%  Learning Rate:{:>.6f}'.format(i+1,epoches,train_loss,train_acc,lr),end = '')
-        time_ = int(end - start)
-        h = time_ / 3600
-        m = time_ % 3600 /60
-        s = time_ % 60
-        time_str = "\tTime %02d:%02d" % ( m, s)
+            print('Epoch [{:>3d}/{:>3d}]  Train Loss:{:>.6f}  Train Acc:{:>3.2f}%  Learning Rate:{:>.6f}'.format(i+1,epochs,train_loss,train_acc,lr))
+
         # ====================== 使用 tensorboard ==================
         if writer is not None:
             writer.add_scalars('Loss', {'train': train_loss,
-                                    'valid': test_loss}, i+1)
+                                    'test': test_loss}, i+1)
             writer.add_scalars('Acc', {'train': train_acc ,
-                                   'valid': test_acc}, i+1)
-            writer.add_scalars('Learning Rate',lr,i+1)
+                                   'test': test_acc}, i+1)
+            writer.add_scalar('Learning Rate',lr,i+1)
         # =========================================================
-        # 打印所用时间
-        print(time_str)
         # 如果取得更好的准确率，就保存模型
         if test_acc > best_acc:
             torch.save(net,path)
@@ -185,121 +173,12 @@ def train(net, trainloader, testloader, epoches, optimizer , criterion, schedule
     Lr = lr_list
     return Acc, Loss, Lr
 
-def train2(net, trainloader, testloader, epoches, optimizer , criterion, scheduler , path = './model.pth', verbose = False):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    best_acc = 0
-    train_acc_list, test_acc_list = [],[]
-    train_loss_list, test_loss_list = [],[]
-    lr_list  = []
-    for i in range(epoches):
-        start = time.time()
-        train_loss = 0
-        train_acc = 0
-        test_loss = 0
-        test_acc = 0
-        if torch.cuda.is_available():
-            net = net.to(device)
-        net.train()
-        for step,data in enumerate(trainloader,start=0):
-            im,label = data
-            im = im.to(device)
-            label = label.to(device)
-
-            optimizer.zero_grad()
-            # 释放内存
-            if hasattr(torch.cuda, 'empty_cache'):
-                torch.cuda.empty_cache()
-            # formard
-            outputs = net(im)
-            loss = criterion(outputs,label)
-            # backward
-            loss.backward()
-            # 更新参数
-            optimizer.step()
-
-            train_loss += loss.data
-            # probs, pred_y = outputs.data.max(dim=1) # 得到概率
-            # # 正确的个数
-            # train_acc += (pred_y==label).sum().item()
-            # # 总数
-            # total += label.size(0)
-            train_acc += get_acc(outputs,label)
-            # 打印下载进度
-            rate = (step + 1) / len(trainloader)
-            a = "*" * int(rate * 50)
-            b = "." * (50 - int(rate * 50))
-            print('\r train {:3d}|{:3d} {:^3.0f}%  [{}->{}] '.format(i+1,epoches,int(rate*100),a,b),end='')
-        train_loss = train_loss / len(trainloader)
-        train_acc = train_acc * 100 / len(trainloader)
-        if verbose:
-            train_acc_list.append(train_acc.item())
-            train_loss_list.append(train_loss.item())
-    #     print('train_loss:{:.6f} train_acc:{:3.2f}%' .format(train_loss ,train_acc),end=' ')  
-        # 记录学习率
-        lr = optimizer.param_groups[0]['lr']
-        if verbose:
-            lr_list.append(lr)
-        # 更新学习率
-        scheduler.step()
-        if testloader is not None:
-            net.eval()
-            with torch.no_grad():
-                for step,data in enumerate(testloader,start=0):
-                    im,label = data
-                    im = im.to(device)
-                    label = label.to(device)
-                    # 释放内存
-                    if hasattr(torch.cuda, 'empty_cache'):
-                        torch.cuda.empty_cache()
-                    outputs = net(im)
-                    loss = criterion(outputs,label)
-                    test_loss += loss.data
-                    # probs, pred_y = outputs.data.max(dim=1) # 得到概率
-                    # test_acc += (pred_y==label).sum().item()
-                    # total += label.size(0)
-                    test_acc += get_acc(outputs,label)
-                    rate = (step + 1) / len(testloader)
-                    a = "*" * int(rate * 50)
-                    b = "." * (50 - int(rate * 50))
-                    print('\r test  {:3d}|{:3d} {:^3.0f}%  [{}->{}] '.format(i+1,epoches,int(rate*100),a,b),end='')
-            test_loss = test_loss / len(testloader)
-            test_acc = test_acc * 100 / len(testloader)
-            if verbose:
-                test_loss_list.append(test_loss.item())
-                test_acc_list.append(test_acc.item())
-            end = time.time()
-            print(
-                '\rEpoch [{:>3d}/{:>3d}]  Train Loss:{:>.6f}  Train Acc:{:>3.2f}% Test Loss:{:>.6f}  Test Acc:{:>3.2f}%  Learning Rate:{:>.6f}'.format(
-                    i + 1, epoches, train_loss, train_acc, test_loss, test_acc,lr), end='')
-        else:
-            end = time.time()
-            print('\rEpoch [{:>3d}/{:>3d}]  Train Loss:{:>.6f}  Train Acc:{:>3.2f}%  Learning Rate:{:>.6f}'.format(i+1,epoches,train_loss,train_acc,lr),end = '')
-        time_ = int(end - start)
-        h = time_ / 3600
-        m = time_ % 3600 /60
-        s = time_ % 60
-        time_str = "\tTime %02d:%02d" % ( m, s)
-        # 答应所用时间
-        print(time_str)
-        # 如果取得更好的准确率，就保存模型
-        if test_acc > best_acc:
-            torch.save(net,path)
-            best_acc = test_acc
-    Acc = {}
-    Loss = {}
-    Acc['train_acc'] = train_acc_list
-    Acc['test_acc'] = test_acc_list
-    Loss['train_loss'] = train_loss_list
-    Loss['test_loss'] = test_loss_list
-    Lr = lr_list
-    return Acc, Loss, Lr
-
-def plot_history(epoches, Acc, Loss, lr):
-    plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
+def plot_history(epochs, Acc, Loss, lr):
+    plt.rcParams['figure.figsize'] = (12.0, 8.0)  # set default size of plots
     plt.rcParams['image.interpolation'] = 'nearest'
     plt.rcParams['image.cmap'] = 'gray'
 
-    epoch_list = range(1,epoches + 1)
+    epoch_list = range(1,epochs + 1)
     plt.plot(epoch_list, Loss['train_loss'])
     plt.plot(epoch_list, Loss['test_loss'])
     plt.xlabel('epoch')
